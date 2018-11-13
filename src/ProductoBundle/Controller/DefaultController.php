c3c1aeb80da00afbf714f0b4609441c0ed25041b
unknown <ejvn3009@gmail.com>
2018-11-12 23:44:36 -0300

    busqueda personalizada de productos con search bar

--- src/ProductoBundle/Controller/DefaultController.php
+++ src/ProductoBundle/Controller/DefaultController.php
@@ -12,8 +12,20 @@ class DefaultController extends Controller
 	*/
     public function indexAction()
     {
-    	//traer todos de la base de datos
-    	$productos = $this->getDoctrine()->getRepository('ProductoBundle:Producto')->findAll();
+    	$request = $this->getRequest()->get('name');
+    	if(!isset($request))
+    	{
+    		$productos = $this->getDoctrine()->getRepository('ProductoBundle:Producto')->findAll();
+    		return $this->render('ProductoBundle:Default:index.html.twig',['productos'=> $productos]);
+    	}
+    	$productos = $this->getDoctrine()->getRepository('ProductoBundle:Producto')->createQueryBuilder('o')
+    								 ->where('o.name LIKE :name')
+    								 ->setParameter('name', '%'.$request.'%')
+    								 ->getQuery()
+    								 ->getResult();
     	return $this->render('ProductoBundle:Default:index.html.twig',['productos'=> $productos]);
+    	//traer todos de la base de datos
+    	// $productos = $this->getDoctrine()->getRepository('ProductoBundle:Producto')->findAll();
+    	// return $this->render('ProductoBundle:Default:index.html.twig',['productos'=> $productos]);
     }
 }
